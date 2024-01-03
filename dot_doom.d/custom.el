0f04c2709eca25d912c95accc41426fb2fa88638
Oystein Straete <oystein@pop-os.localdomain>
2024-01-03 08:59:44 +0100

    add doom and nvim

---
+++ dot_doom.d/custom.el
@@ -0,0 +1,25 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(warning-suppress-types
+   '((org-element-cache)
+     (org-element-cache)
+     (org-element-cache)) t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Copy current git branch to kill ring
+(defun magit-copy-current-branch-to-kill-ring ()
+  "Show the current branch in the echo-area and add it to the `kill-ring'."
+  (interactive)
+  (let ((branch (magit-get-current-branch)))
+    (if branch
+        (progn (kill-new branch)
+               (message "%s" branch))
+      (user-error "There is not current branch"))))
---
+++ private_dot_config/nvim/init.vim
@@ -0,0 +1,5 @@
+set ignorecase
+set number
+set ts=2 sw=2
+set clipboard+=unnamedplus
+syntax on
